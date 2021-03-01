2b70d2db7e11ec7062058fd1735ca184fa283a87
JuanDavid <jdmv161010@gmail.com>
2021-03-01 11:29:10 -0500

    Implementando Yargs

--- multiplicar/multiplicar.js
+++ multiplicar/multiplicar.js
@@ -3,9 +3,6 @@ const { rejects } = require('assert');
 const fs = require('fs');
 const { resolve } = require('path');
 
-
-
-
 let crearArchivo = ( base ) => {
     return new Promise ( (resolve, reject) => {
         
